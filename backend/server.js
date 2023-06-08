794f60e11195ec14ea555877174799fc7a75ce32
umairayub79 <umairayub79@gmail.com>
2023-06-09 01:17:35 +0500

    setup mongo db, connect db

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,28 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const routes = require('./routes/workouts')
 
 const app = express()
 
 app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
-app.use('/api/workouts',routes)
+app.use('/api/workouts', routes)
 
 
-app.listen(process.env.PORT, () => {
-    console.log('running on port', process.env.PORT)
+// connect to db
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log('connected to db & running on port', process.env.PORT)
+    })
+}
+).catch((error) => {
+    console.log(error);
 })
+
