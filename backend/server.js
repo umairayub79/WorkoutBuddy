e955e3873afb2d3e744ff06224152aaf16b6105f
umairayub79 <umairayub79@gmail.com>
2023-06-09 21:08:31 +0500

    feat: Connect frontend to backend and display list of items
    - Implemented API requests to fetch a list of items from the backend
    - Updated the home page component to display the fetched items

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,12 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes/workouts')
+const cors = require('cors');
 
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import WorkoutDetails from '../components/WorkoutDetails';
 
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null)
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/api/workouts");
+        const { data } = response;
+
+        if (response.status === 200) {
+          setWorkouts(data);
+        }
+      } catch (error) {
+        // Handle error here
+        console.error(error);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts && workouts.map((workout) => (
+          <WorkoutDetails key={workout._id} workout={workout}/>
+        ))}
+      </div>
     </div>
   )
 }
